ce2399fdb480b20eaeaa45afd2b27c4d629a2a00
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-03-03 21:25:08 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,48 @@ from collections import defaultdict
 from collections import deque
 
 
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#
+#
+# class Solution:
+#     def removeNthFromEnd(self, head, n):
+#         slow, fast = head, head
+#         for _ in range(n):
+#             fast = fast.next
+#         if not fast:
+#             return head.next
+#         while fast.next:
+#             slow = slow.next
+#             fast = fast.next
+#         slow.next = slow.next.next
+#         return head
+#
+#
+# def printList(head):
+#     """Функция для печати элементов связного списка"""
+#     current = head
+#     while current:
+#         print(current.val, end=' ')
+#         current = current.next
+#     print()
+#
+#
+# # Создание связного списка [1, 2, 3, 4, 5]
+# head = ListNode(1)
+# head.next = ListNode(2)
+# head.next.next = ListNode(3)
+# head.next.next.next = ListNode(4)
+# head.next.next.next.next = ListNode(5)
+#
+# solution = Solution()
+# n = 2
+# head = solution.removeNthFromEnd(head, n)
+# printList(head)
+
+
 # def sortedSquares(nums):
 #     result = [i ** 2 for i in nums]
 #     return sorted(result)
