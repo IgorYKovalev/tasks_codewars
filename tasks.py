2b9b1d3bd7b1f550da82aa8ba0ad12a00737473a
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-03-24 18:40:25 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,19 @@ from collections import defaultdict
 from collections import deque
 
 
+# def findDuplicate(nums):
+#     seen = set()
+#     for num in nums:
+#         if num in seen:
+#             return num
+#         seen.add(num)
+#
+#
+# print(findDuplicate([1, 3, 4, 2, 2]))
+# print(findDuplicate([3, 1, 3, 4, 2]))
+# print(findDuplicate([3, 3, 3, 3, 3]))
+
+
 # class ListNode:
 #     def __init__(self, value=0, next=None):
 #         self.value = value
