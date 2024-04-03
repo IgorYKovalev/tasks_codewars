7d7fb303f47146e15625d80ae118af18f7ba85db
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-03 17:30:26 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,47 @@ from collections import defaultdict
 from collections import deque
 
 
+# def exist(board, word):
+#     if not board:
+#         return False
+#
+#     def dfs(i, j, word_index):
+#         if word_index == len(word):
+#             return True
+#
+#         if i < 0 or i >= len(board) or \
+#             j < 0 or j >= len(board[0]) or \
+#                 board[i][j] != word[word_index]:
+#             return False
+#
+#         temp = board[i][j]
+#         board[i][j] = ''  # Помечаем текущую ячейку как посещенную
+#
+#         # Проверяем соседние ячейки в четырех направлениях
+#         found = \
+#             dfs(i + 1, j, word_index + 1) or \
+#             dfs(i - 1, j, word_index + 1) or \
+#             dfs(i, j + 1, word_index + 1) or \
+#             dfs(i, j - 1, word_index + 1)
+#
+#         board[i][j] = temp  # Восстанавливаем текущую ячейку
+#
+#         return found
+#
+#     # Начинаем поиск слова с каждой ячейки сетки
+#     for i in range(len(board)):
+#         for j in range(len(board[0])):
+#             if dfs(i, j, 0):
+#                 return True
+#
+#     return False
+#
+#
+# print(exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"))
+# print(exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE"))
+# print(exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB"))
+
+
 # def isIsomorphic(s, t):
 #     # result_s = [(s.count(k), len(list(v))) for k, v in groupby(s)]
 #     # result_t = [(t.count(k), len(list(v))) for k, v in groupby(t)]
