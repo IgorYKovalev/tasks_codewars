e358a204d42bf6260926925831cd3f51a74df4e5
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-07 13:36:42 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,30 @@ from collections import defaultdict
 from collections import deque
 
 
+# def checkValidString(s):
+#     leftMin, leftMax = 0, 0
+#     for i in s:
+#         if i == '(':
+#             leftMin, leftMax = leftMin + 1, leftMax + 1
+#         elif i == ')':
+#             leftMin, leftMax = leftMin - 1, leftMax - 1
+#         else:
+#             leftMin, leftMax = leftMin - 1, leftMax + 1
+#         if leftMax < 0:
+#             return False
+#         if leftMin < 0:
+#             leftMin = 0
+#
+#     return leftMin == 0
+#
+#
+# print(checkValidString("()"))
+# print(checkValidString("(*)"))
+# print(checkValidString("(*))"))
+# print(checkValidString("("))
+# print(checkValidString("((((((("))
+
+
 # def minRemoveToMakeValid(s):
 #     stack = []
 #     to_remove = set()
