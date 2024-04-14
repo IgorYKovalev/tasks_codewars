3f769d6eb64b18e7358249bee074dbbbb6964b4a
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-14 15:12:23 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -1695,6 +1695,31 @@ from collections import deque
 # print(solution.diameterOfBinaryTree(root))
 
 
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# def sumOfLeftLeaves(root):
+#     def dfs(node, is_left):
+#         if not node:
+#             return 0
+#         if not node.left and not node.right:
+#             return node.val if is_left else 0
+#         return dfs(node.left, True) + dfs(node.right, False)
+#     return dfs(root, False)
+#
+#
+# root = TreeNode(3)
+# root.left = TreeNode(9)
+# root.right = TreeNode(20)
+# root.right.left = TreeNode(15)
+# root.right.right = TreeNode(7)
+# print(sumOfLeftLeaves(root))
+
+
 # class Node:
 #     def __init__(self, L, R, n):
 #         self.left = L
