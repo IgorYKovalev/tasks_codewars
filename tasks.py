3f59e7dc8f82101561b556aa56d01f923b3b19f2
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-15 15:56:46 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -1740,6 +1740,43 @@ from collections import deque
 # print(tree_by_sum(res))
 
 
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# class Solution(object):
+#     def sumNumbers(self, root):
+#         def dfs(node, current_sum):
+#             if not node:
+#                 return 0
+#
+#             current_sum = current_sum * 10 + node.val
+#             print(current_sum)
+#             if not node.left and not node.right:
+#                 return current_sum
+#
+#             left_sum = dfs(node.left, current_sum)
+#             right_sum = dfs(node.right, current_sum)
+#
+#             return left_sum + right_sum
+#
+#         return dfs(root, 0)
+#
+#
+# root = TreeNode(4)
+# root.left = TreeNode(9)
+# root.right = TreeNode(0)
+# root.left.left = TreeNode(5)
+# root.left.right = TreeNode(1)
+#
+#
+# solution = Solution()
+# print(solution.sumNumbers(root))
+
+
 # class Node:
 #     def __init__(self, L, R, n):
 #         self.left = L
