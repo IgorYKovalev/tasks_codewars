5048af084cff8589aed539470fc378855d7a20c1
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-17 19:52:25 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -1728,6 +1728,43 @@ from collections import deque
 #
 #
 # class Solution(object):
+#     def smallestFromLeaf(self, root):
+#         self.result = []
+#
+#         def dfs(node, res):
+#             if not node.left and not node.right:
+#                 res += chr(ord('a') + node.val)
+#                 self.result.append(res[::-1])
+#             if node.left:
+#                 dfs(node.left, res + chr(ord('a') + node.val))
+#             if node.right:
+#                 dfs(node.right, res + chr(ord('a') + node.val))
+#
+#         dfs(root, '')
+#         self.result.sort()
+#         return self.result[0]
+#
+#
+# root = TreeNode(25)
+# root.left = TreeNode(1)
+# root.right = TreeNode(3)
+# root.left.left = TreeNode(1)
+# root.left.right = TreeNode(3)
+# root.right.left = TreeNode(0)
+# root.right.right = TreeNode(2)
+#
+# solution = Solution()
+# print(solution.smallestFromLeaf(root))
+
+
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# class Solution(object):
 #     def addOneRow(self, root, val, depth):
 #         if depth == 1:
 #             new_root = TreeNode(val)
