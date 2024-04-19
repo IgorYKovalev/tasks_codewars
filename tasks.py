a44d254862aa5c2056b7dfc88b9a93a132aeea54
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-04-19 19:23:00 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,37 @@ from collections import defaultdict
 from collections import deque
 
 
+# def numIslands(grid):
+#     if not grid:
+#         return 0
+#     rows, cols = len(grid), len(grid[0])
+#     islands = 0
+#
+#     def dfs(i, j):
+#         if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != '1':
+#             return
+#
+#         grid[i][j] = '0'
+#         dfs(i - 1, j)
+#         dfs(i + 1, j)
+#         dfs(i, j - 1)
+#         dfs(i, j + 1)
+#
+#     for i in range(rows):
+#         for j in range(cols):
+#             if grid[i][j] == "1":
+#                 dfs(i, j)
+#                 islands += 1
+#
+#     return islands
+#
+#
+# print(numIslands([["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]))
+# print(numIslands([["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]]))
+# print(numIslands([["0"]]))
+# print(numIslands([["1", "1"]]))
+
+
 # def islandPerimeter(grid):
 #     rows = len(grid)
 #     cols = len(grid[0])
