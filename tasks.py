b4af2e3b49d87d856913382e42cc5b2f287123c0
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-05-12 19:27:12 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,20 @@ from collections import defaultdict
 from collections import deque
 
 
+# def largestLocal(grid):
+#     n = len(grid)
+#     ans = [[0] * (n - 2) for _ in range(n - 2)]
+#     for i in range(n - 2):
+#         for j in range(n - 2):
+#             ans[i][j] = max(
+#                 grid[ii][jj] for ii in range(i, i + 3) for jj in range(j, j + 3)
+#             )
+#     return ans
+#
+#
+# print(largestLocal([[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]]))
+
+
 # def mincostToHireWorkers(quality, wage, k):
 #     workers = sorted((w / q, q) for w, q in zip(wage, quality))
 #     maxheap = []
