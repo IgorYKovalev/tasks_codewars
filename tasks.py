2ce8d37f31432097660cedcc19c9ceef40e049ea
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-05-17 21:06:45 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -1393,6 +1393,24 @@ from collections import deque
 #         node.next = node.next.next
 
 
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# class Solution:
+#     def removeLeafNodes(self, root, target):
+#         if not root:
+#             return None
+#         root.left = self.removeLeafNodes(root.left, target)
+#         root.right = self.removeLeafNodes(root.right, target)
+#         if not root.left and not root.right and root.val == target:
+#             return None
+#         return root
+
+
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
