307fd04e3083777c925e2dc92e8fb4ed255033a8
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-05-18 21:12:33 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -1381,6 +1381,26 @@ from collections import deque
 # printList(middleNode)
 
 
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+#
+# class Solution:
+#     def distributeCoins(self, root):
+#         def f(root, parent):
+#             if not root: return 0
+#             moves = f(root.left, root) + f(root.right, root)
+#             x = root.val - 1
+#             if parent != None: parent.val += x
+#             moves += abs(x)
+#             return moves
+#
+#         return f(root, None)
+
+
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
