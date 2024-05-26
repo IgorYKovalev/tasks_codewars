ab22f883b8ff94b3c6c9e6db01e5528ec194e8c0
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2024-05-26 15:50:35 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,26 @@ from collections import defaultdict
 from collections import deque
 
 
+# def checkRecord(n):
+#     dp = [[[-1] * 3 for _ in range(2)] for _ in range(n + 1)]
+#     mod = 10 ** 9 + 7
+#
+#     def f(i, absent, late):
+#         if absent >= 2 or late >= 3: return 0
+#         if i == 0: return 1
+#         if dp[i][absent][late] != -1:
+#             return dp[i][absent][late]
+#         ans = f(i - 1, absent, 0)
+#         ans += f(i - 1, absent, late + 1)
+#         ans += f(i - 1, absent + 1, 0)
+#         dp[i][absent][late] = ans % mod
+#         return dp[i][absent][late]
+#     return f(n, 0, 0)
+#
+#
+# print(checkRecord(2))
+
+
 # def wordBreak(s, wordDict):
 #
 #     def f(i):
