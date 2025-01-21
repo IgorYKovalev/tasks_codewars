9ff56f4730db2a416e55003d0482cb18dacd56c7
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-01-21 19:03:24 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -15,6 +15,26 @@ from collections import defaultdict
 from collections import deque
 
 
+# class Solution:
+#     def gridGame(self, grid):
+#         min_result = float('inf')
+#         row1_sum = sum(grid[0])
+#         row2_sum = 0
+#
+#         for i in range(len(grid[0])):
+#             row1_sum -= grid[0][i]
+#             min_result = min(min_result, max(row1_sum, row2_sum))
+#             row2_sum += grid[1][i]
+#
+#         return min_result
+#
+#
+# grid = [[2, 5, 4], [1, 5, 1]]
+# solution = Solution()
+# result = solution.gridGame(grid)
+# print(result)
+
+
 # def scoreOfString(s):
 #     result = []
 #     asc = [ord(i) for i in s]
