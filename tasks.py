c2c7cbda92316723d7dae0b9480551b71d4d58ea
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-01-22 23:48:20 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -16,6 +16,38 @@ from collections import deque
 
 
 # class Solution:
+#     def highestPeak(self, isWater):
+#         R = len(isWater)
+#         C = len(isWater[0])
+#         height = [[float('inf')] * C for _ in range(R)]
+#
+#         for i in range(R):
+#             for j in range(C):
+#                 if isWater[i][j] == 1:
+#                     height[i][j] = 0
+#                 else:
+#                     if i > 0:
+#                         height[i][j] = min(height[i][j], height[i - 1][j] + 1)
+#                     if j > 0:
+#                         height[i][j] = min(height[i][j], height[i][j - 1] + 1)
+#
+#         for i in range(R - 1, -1, -1):
+#             for j in range(C - 1, -1, -1):
+#                 if i < R - 1:
+#                     height[i][j] = min(height[i][j], height[i + 1][j] + 1)
+#                 if j < C - 1:
+#                     height[i][j] = min(height[i][j], height[i][j + 1] + 1)
+#
+#         return height
+#
+#
+# isWater = [[0,0,1],[1,0,0],[0,0,0]]
+# solution = Solution()
+# result = solution.highestPeak(isWater)
+# print(result)
+
+
+# class Solution:
 #     def gridGame(self, grid):
 #         min_result = float('inf')
 #         row1_sum = sum(grid[0])
