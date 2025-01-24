79c7e5457e757e79b0405fe9bd112a818ae18e92
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-01-24 22:17:32 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -13,6 +13,40 @@ import math
 import random
 from collections import defaultdict
 from collections import deque
+from typing import List
+
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        res = []
+        visited = set()
+        memo = [0] * n
+
+        def dfs(i):
+            if memo[i] == 1 or len(graph[i]) == 0:
+                return True
+            elif memo[i] == -1 or i in visited:
+                return False
+            visited.add(i)
+
+            for neighbour in graph[i]:
+                if not dfs(neighbour):
+                    memo[i] = -1
+                    return False
+            memo[i] = 1
+            return True
+
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        return res
+
+
+graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
+solution = Solution()
+result = solution.eventualSafeNodes(graph)
+print(result)
 
 
 # class Solution:
