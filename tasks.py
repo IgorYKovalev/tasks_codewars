a7b5bb228ac33827bd884df456af1c41c3f565f2
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-03-08 12:58:08 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -17,34 +17,56 @@ import heapq
 
 
 class Solution:
-    def closestPrimes(self, left: int, right: int) -> list[int]:
-        sieve = [True] * (right + 1)
-        sieve[0] = sieve[1] = False
-        for i in range(2, int(right ** 0.5) + 1):
-            if sieve[i]:
-                for j in range(i * i, right + 1, i):
-                    sieve[j] = False
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        count = 0
+        res = float("inf")
 
-        primes = [i for i in range(left, right + 1) if sieve[i]]
-        if len(primes) < 2:
-            return [-1, -1]
+        for i in range(len(blocks)):
+            if i - k >= 0 and blocks[i - k] == 'B':
+                count -= 1
+            if blocks[i] == 'B':
+                count += 1
+            res = min(res, k - count)
 
-        min_gap = float('inf')
-        result = [-1, -1]
-        for i in range(1, len(primes)):
-            gap = primes[i] - primes[i - 1]
-            if gap < min_gap:
-                min_gap = gap
-                result = [primes[i - 1], primes[i]]
-
-        return result
+        return res
 
 
-left = 10
-right = 19
+blocks = "WBBWWBBWBW"
+k = 7
 solution = Solution()
-result = solution.closestPrimes(left, right)
+result = solution.minimumRecolors(blocks, k)
 print(result)
+
+
+# class Solution:
+#     def closestPrimes(self, left: int, right: int) -> list[int]:
+#         sieve = [True] * (right + 1)
+#         sieve[0] = sieve[1] = False
+#         for i in range(2, int(right ** 0.5) + 1):
+#             if sieve[i]:
+#                 for j in range(i * i, right + 1, i):
+#                     sieve[j] = False
+#
+#         primes = [i for i in range(left, right + 1) if sieve[i]]
+#         if len(primes) < 2:
+#             return [-1, -1]
+#
+#         min_gap = float('inf')
+#         result = [-1, -1]
+#         for i in range(1, len(primes)):
+#             gap = primes[i] - primes[i - 1]
+#             if gap < min_gap:
+#                 min_gap = gap
+#                 result = [primes[i - 1], primes[i]]
+#
+#         return result
+#
+#
+# left = 10
+# right = 19
+# solution = Solution()
+# result = solution.closestPrimes(left, right)
+# print(result)
 
 
 # class Solution:
