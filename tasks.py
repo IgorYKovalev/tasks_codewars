30c8c5303a3e4444b2b2f8ab07fbe0761be4cc46
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-03-20 18:50:08 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -17,24 +17,63 @@ import heapq
 
 
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        k = 0
+    def minimumCost(self, n, edges, query):
+        parent = list(range(n))
+        min_path_cost = [-1] * n
 
-        for i in range(n - 2):
-            if nums[i] == 0:
-                nums[i] ^= 1
-                nums[i + 1] ^= 1
-                nums[i + 2] ^= 1
-                k += 1
+        def find_root(node):
+            if parent[node] != node:
+                parent[node] = find_root(parent[node])
+            return parent[node]
 
-        return -1 if 0 in nums else k
+        for source, target, weight in edges:
+            source_root = find_root(source)
+            target_root = find_root(target)
+            min_path_cost[target_root] &= weight
+
+            if source_root != target_root:
+                min_path_cost[target_root] &= min_path_cost[source_root]
+                parent[source_root] = target_root
+
+        result = []
+        for start, end in query:
+            if start == end:
+                result.append(0)
+            elif find_root(start) != find_root(end):
+                result.append(-1)
+            else:
+                result.append(min_path_cost[find_root(start)])
+
+        return result
 
 
-nums = [0, 1, 1, 1, 0, 0]
+n = 5
+edges = [[0, 1, 7], [1, 3, 7], [1, 2, 1]]
+query = [[0, 3], [3, 4]]
 solution = Solution()
-result = solution.minOperations(nums)
+result = solution.minimumCost(n, edges, query)
 print(result)
+
+
+# class Solution:
+#     def minOperations(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         k = 0
+#
+#         for i in range(n - 2):
+#             if nums[i] == 0:
+#                 nums[i] ^= 1
+#                 nums[i + 1] ^= 1
+#                 nums[i + 2] ^= 1
+#                 k += 1
+#
+#         return -1 if 0 in nums else k
+#
+#
+# nums = [0, 1, 1, 1, 0, 0]
+# solution = Solution()
+# result = solution.minOperations(nums)
+# print(result)
 
 
 # class Solution:
