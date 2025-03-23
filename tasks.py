842493fb0f83575d8fbd401b212b355abb482eed
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-03-23 12:59:11 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -17,39 +17,76 @@ import heapq
 
 
 class Solution:
-    def countCompleteComponents(self, n, edges):
-        from collections import deque
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-        vis = [False] * n
-        ans = 0
-        def bfs(node):
-            q = deque([node])
-            vis[node] = True
-            comp = []
-            while q:
-                cur = q.popleft()
-                comp.append(cur)
-                for neighbor in adj[cur]:
-                    if not vis[neighbor]:
-                        vis[neighbor] = True
-                        q.append(neighbor)
-            return comp
-        for i in range(n):
-            if not vis[i]:
-                comp = bfs(i)
-                if all(len(adj[node]) == len(comp) - 1 for node in comp):
-                    ans += 1
-        return ans
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for u, v, time in roads:
+            graph[u].append((v, time))
+            graph[v].append((u, time))
+
+        MOD = 10 ** 9 + 7
+        distance = [float('inf')] * n
+        count = [0] * n
+        distance[0] = 0
+        count[0] = 1
+        priorityQueue = [(0,0)]
+
+        while priorityQueue:
+            curr, u = heapq.heappop(priorityQueue)
+            if curr > distance[u]:
+                continue
+            for v, time in graph[u]:
+                new_path = curr + time
+                if new_path < distance[v]:
+                    distance[v] = new_path
+                    count[v] = count[u]
+                    heapq.heappush(priorityQueue, (new_path, v))
+                elif new_path == distance[v]:
+                    count[v] += count[u]
+                    count[v] %= MOD
+        return count[n - 1]
 
 
-n = 6
-edges = [[0,1],[0,2],[1,2],[3,4]]
+n = 7
+roads = [[0,6,7], [0,1,2], [1,2,3], [1,3,3], [6,3,3], [3,5,1], [6,5,1], [2,5,1], [0,4,5], [4,6,2]]
 solution = Solution()
-result = solution.countCompleteComponents(n, edges)
+result = solution.countPaths(n, roads)
 print(result)
+
+
+# class Solution:
+#     def countCompleteComponents(self, n, edges):
+#         from collections import deque
+#         adj = [[] for _ in range(n)]
+#         for u, v in edges:
+#             adj[u].append(v)
+#             adj[v].append(u)
+#         vis = [False] * n
+#         ans = 0
+#         def bfs(node):
+#             q = deque([node])
+#             vis[node] = True
+#             comp = []
+#             while q:
+#                 cur = q.popleft()
+#                 comp.append(cur)
+#                 for neighbor in adj[cur]:
+#                     if not vis[neighbor]:
+#                         vis[neighbor] = True
+#                         q.append(neighbor)
+#             return comp
+#         for i in range(n):
+#             if not vis[i]:
+#                 comp = bfs(i)
+#                 if all(len(adj[node]) == len(comp) - 1 for node in comp):
+#                     ans += 1
+#         return ans
+#
+#
+# n = 6
+# edges = [[0,1],[0,2],[1,2],[3,4]]
+# solution = Solution()
+# result = solution.countCompleteComponents(n, edges)
+# print(result)
 
 
 # class Solution:
