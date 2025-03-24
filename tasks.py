fa3229ab1543e03bb6c7cbae542e9f70ad7d4421
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-03-24 20:49:54 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -17,40 +17,61 @@ import heapq
 
 
 class Solution:
-    def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for u, v, time in roads:
-            graph[u].append((v, time))
-            graph[v].append((u, time))
+    def countDays(self, days, meetings):
+        meetings.sort()
+        prevEnd = 0
 
-        MOD = 10 ** 9 + 7
-        distance = [float('inf')] * n
-        count = [0] * n
-        distance[0] = 0
-        count[0] = 1
-        priorityQueue = [(0,0)]
+        for start, end in meetings:
+            start = max(start, prevEnd + 1)
+            length = end - start + 1
+            days -= max(length, 0)
+            prevEnd = max(prevEnd, end)
 
-        while priorityQueue:
-            curr, u = heapq.heappop(priorityQueue)
-            if curr > distance[u]:
-                continue
-            for v, time in graph[u]:
-                new_path = curr + time
-                if new_path < distance[v]:
-                    distance[v] = new_path
-                    count[v] = count[u]
-                    heapq.heappush(priorityQueue, (new_path, v))
-                elif new_path == distance[v]:
-                    count[v] += count[u]
-                    count[v] %= MOD
-        return count[n - 1]
+        return days
 
 
-n = 7
-roads = [[0,6,7], [0,1,2], [1,2,3], [1,3,3], [6,3,3], [3,5,1], [6,5,1], [2,5,1], [0,4,5], [4,6,2]]
+days = 10
+meetings = [[5,7], [1,3], [9,10]]
 solution = Solution()
-result = solution.countPaths(n, roads)
+result = solution.countDays(days, meetings)
 print(result)
+
+
+# class Solution:
+#     def countPaths(self, n: int, roads: List[List[int]]) -> int:
+#         graph = defaultdict(list)
+#         for u, v, time in roads:
+#             graph[u].append((v, time))
+#             graph[v].append((u, time))
+#
+#         MOD = 10 ** 9 + 7
+#         distance = [float('inf')] * n
+#         count = [0] * n
+#         distance[0] = 0
+#         count[0] = 1
+#         priorityQueue = [(0,0)]
+#
+#         while priorityQueue:
+#             curr, u = heapq.heappop(priorityQueue)
+#             if curr > distance[u]:
+#                 continue
+#             for v, time in graph[u]:
+#                 new_path = curr + time
+#                 if new_path < distance[v]:
+#                     distance[v] = new_path
+#                     count[v] = count[u]
+#                     heapq.heappush(priorityQueue, (new_path, v))
+#                 elif new_path == distance[v]:
+#                     count[v] += count[u]
+#                     count[v] %= MOD
+#         return count[n - 1]
+#
+#
+# n = 7
+# roads = [[0,6,7], [0,1,2], [1,2,3], [1,3,3], [6,3,3], [3,5,1], [6,5,1], [2,5,1], [0,4,5], [4,6,2]]
+# solution = Solution()
+# result = solution.countPaths(n, roads)
+# print(result)
 
 
 # class Solution:
