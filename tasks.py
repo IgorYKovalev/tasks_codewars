0f8ca41178887d4af4e08cb229413be0826496c3
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-04-12 21:02:14 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -16,22 +16,74 @@ from typing import List, Optional
 
 
 class Solution:
-    def countSymmetricIntegers(self, low: int, high: int) -> int:
-        count = 0
-        for num in range(low, high + 1):
-            s = str(num)
-            if len(s) % 2 == 0:
-                mid = len(s) // 2
-                if sum(map(int, s[:mid])) == sum(map(int, s[mid:])):
-                    count += 1
-        return count
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        if n == 1:
+            total = 0
+            for i in range(1, 10):
+                if i % k == 0:
+                    total += 1
+            return total
+
+        fact = [1]
+        for i in range(1, n + 1):
+            fact.append(fact[-1] * i)
+
+        seen = set()
+        ans = 0
+        for left in range(10 ** ((n - 1) // 2), 10 ** ((n + 1) // 2)):
+            l = str(left)
+            r = l[::-1]
+            if n % 2 == 1:
+                r = r[1:]
+            t = l + r
+
+            if int(t) % k != 0:
+                continue
+
+            s = "".join(sorted(list(t)))
+            if s in seen:
+                continue
+            seen.add(s)
+            count = collections.Counter(t)
+            total = fact[n]
+            for key in count.keys():
+                total //= fact[count[key]]
+
+            ans += total
+            if count["0"] >= 1:
+                total_non_zero = fact[n - 1]
+                count["0"] -= 1
+                for key in count.keys():
+                    total_non_zero //= fact[count[key]]
+                ans -= total_non_zero
+
+        return ans
 
 
-low = 1
-high = 100
+n = 3
+k = 5
 solution = Solution()
-result = solution.countSymmetricIntegers(low, high)
+result = solution.countGoodIntegers(n, k)
 print(result)
+
+
+# class Solution:
+#     def countSymmetricIntegers(self, low: int, high: int) -> int:
+#         count = 0
+#         for num in range(low, high + 1):
+#             s = str(num)
+#             if len(s) % 2 == 0:
+#                 mid = len(s) // 2
+#                 if sum(map(int, s[:mid])) == sum(map(int, s[mid:])):
+#                     count += 1
+#         return count
+#
+#
+# low = 1
+# high = 100
+# solution = Solution()
+# result = solution.countSymmetricIntegers(low, high)
+# print(result)
 
 
 # class Solution:
