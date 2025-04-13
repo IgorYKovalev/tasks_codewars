08fd17bd0965000272c713a6c9d51087c6ba06e2
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-04-13 22:25:01 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -16,55 +16,69 @@ from typing import List, Optional
 
 
 class Solution:
-    def countGoodIntegers(self, n: int, k: int) -> int:
-        if n == 1:
-            total = 0
-            for i in range(1, 10):
-                if i % k == 0:
-                    total += 1
-            return total
-
-        fact = [1]
-        for i in range(1, n + 1):
-            fact.append(fact[-1] * i)
-
-        seen = set()
-        ans = 0
-        for left in range(10 ** ((n - 1) // 2), 10 ** ((n + 1) // 2)):
-            l = str(left)
-            r = l[::-1]
-            if n % 2 == 1:
-                r = r[1:]
-            t = l + r
-
-            if int(t) % k != 0:
-                continue
-
-            s = "".join(sorted(list(t)))
-            if s in seen:
-                continue
-            seen.add(s)
-            count = collections.Counter(t)
-            total = fact[n]
-            for key in count.keys():
-                total //= fact[count[key]]
-
-            ans += total
-            if count["0"] >= 1:
-                total_non_zero = fact[n - 1]
-                count["0"] -= 1
-                for key in count.keys():
-                    total_non_zero //= fact[count[key]]
-                ans -= total_non_zero
-
-        return ans
+    def countGoodNumbers(self, n: int) -> int:
+        mod = 10 ** 9 + 7
+        even = (n + 1) // 2
+        odd = n // 2
+        return (pow(5, even, mod) * pow(4, odd, mod)) % mod
 
 
-n = 3
-k = 5
+n = 1
 solution = Solution()
-result = solution.countGoodIntegers(n, k)
+result = solution.countGoodNumbers(n)
 print(result)
+
+
+# class Solution:
+#     def countGoodIntegers(self, n: int, k: int) -> int:
+#         if n == 1:
+#             total = 0
+#             for i in range(1, 10):
+#                 if i % k == 0:
+#                     total += 1
+#             return total
+#
+#         fact = [1]
+#         for i in range(1, n + 1):
+#             fact.append(fact[-1] * i)
+#
+#         seen = set()
+#         ans = 0
+#         for left in range(10 ** ((n - 1) // 2), 10 ** ((n + 1) // 2)):
+#             l = str(left)
+#             r = l[::-1]
+#             if n % 2 == 1:
+#                 r = r[1:]
+#             t = l + r
+#
+#             if int(t) % k != 0:
+#                 continue
+#
+#             s = "".join(sorted(list(t)))
+#             if s in seen:
+#                 continue
+#             seen.add(s)
+#             count = collections.Counter(t)
+#             total = fact[n]
+#             for key in count.keys():
+#                 total //= fact[count[key]]
+#
+#             ans += total
+#             if count["0"] >= 1:
+#                 total_non_zero = fact[n - 1]
+#                 count["0"] -= 1
+#                 for key in count.keys():
+#                     total_non_zero //= fact[count[key]]
+#                 ans -= total_non_zero
+#
+#         return ans
+#
+#
+# n = 3
+# k = 5
+# solution = Solution()
+# result = solution.countGoodIntegers(n, k)
+# print(result)
 
 
 # class Solution:
