a6bcc1bd8f29dd004748be43c08a3bb323d48c7c
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-04-15 18:17:28 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -16,17 +16,59 @@ from typing import List, Optional
 
 
 class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        mod = 10 ** 9 + 7
-        even = (n + 1) // 2
-        odd = n // 2
-        return (pow(5, even, mod) * pow(4, odd, mod)) % mod
+    def goodTriplets(self, nums1, nums2):
+        n = len(nums1)
+        pos = [0] * n
+        for i in range(n):
+            pos[nums2[i]] = i
+        nums1 = [pos[x] for x in nums1]
+        bit1 = [0] * (n + 2)
+        bit2 = [0] * (n + 2)
+
+        def update(bit, i, val):
+            i += 1
+            while i <= n:
+                bit[i] += val
+                i += i & -i
+
+        def query(bit, i):
+            i += 1
+            res = 0
+            while i > 0:
+                res += bit[i]
+                i -= i & -i
+            return res
+
+        ans = 0
+        for i in reversed(range(n)):
+            x = nums1[i]
+            val = query(bit1, n - 1) - query(bit1, x)
+            trip = query(bit2, n - 1) - query(bit2, x)
+            ans += trip
+            update(bit2, x, val)
+            update(bit1, x, 1)
+        return ans
 
 
-n = 1
+nums1 = [2,0,1,3]
+nums2 = [0,1,2,3]
 solution = Solution()
-result = solution.countGoodNumbers(n)
+result = solution.goodTriplets(nums1, nums2)
 print(result)
+
+
+# class Solution:
+#     def countGoodNumbers(self, n: int) -> int:
+#         mod = 10 ** 9 + 7
+#         even = (n + 1) // 2
+#         odd = n // 2
+#         return (pow(5, even, mod) * pow(4, odd, mod)) % mod
+#
+#
+# n = 1
+# solution = Solution()
+# result = solution.countGoodNumbers(n)
+# print(result)
 
 
 # class Solution:
