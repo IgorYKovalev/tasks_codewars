8a6e22b714da2c724d4f67e8adafecb32135aec4
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-04-16 19:04:32 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -16,45 +16,68 @@ from typing import List, Optional
 
 
 class Solution:
-    def goodTriplets(self, nums1, nums2):
-        n = len(nums1)
-        pos = [0] * n
-        for i in range(n):
-            pos[nums2[i]] = i
-        nums1 = [pos[x] for x in nums1]
-        bit1 = [0] * (n + 2)
-        bit2 = [0] * (n + 2)
-
-        def update(bit, i, val):
-            i += 1
-            while i <= n:
-                bit[i] += val
-                i += i & -i
-
-        def query(bit, i):
-            i += 1
-            res = 0
-            while i > 0:
-                res += bit[i]
-                i -= i & -i
-            return res
-
-        ans = 0
-        for i in reversed(range(n)):
-            x = nums1[i]
-            val = query(bit1, n - 1) - query(bit1, x)
-            trip = query(bit2, n - 1) - query(bit2, x)
-            ans += trip
-            update(bit2, x, val)
-            update(bit1, x, 1)
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        freq = defaultdict(int)
+        ans, cnt, l = 0, 0, 0
+        for r, x in enumerate(nums):
+            cnt += freq[x]
+            freq[x] += 1
+            while cnt >= k:
+                ans += n - r
+                freq[nums[l]] -= 1
+                cnt -= freq[nums[l]]
+                l += 1
         return ans
 
 
-nums1 = [2,0,1,3]
-nums2 = [0,1,2,3]
+nums = [1,1,1,1,1]
+k = 10
 solution = Solution()
-result = solution.goodTriplets(nums1, nums2)
+result = solution.countGood(nums, k)
 print(result)
+
+
+# class Solution:
+#     def goodTriplets(self, nums1, nums2):
+#         n = len(nums1)
+#         pos = [0] * n
+#         for i in range(n):
+#             pos[nums2[i]] = i
+#         nums1 = [pos[x] for x in nums1]
+#         bit1 = [0] * (n + 2)
+#         bit2 = [0] * (n + 2)
+#
+#         def update(bit, i, val):
+#             i += 1
+#             while i <= n:
+#                 bit[i] += val
+#                 i += i & -i
+#
+#         def query(bit, i):
+#             i += 1
+#             res = 0
+#             while i > 0:
+#                 res += bit[i]
+#                 i -= i & -i
+#             return res
+#
+#         ans = 0
+#         for i in reversed(range(n)):
+#             x = nums1[i]
+#             val = query(bit1, n - 1) - query(bit1, x)
+#             trip = query(bit2, n - 1) - query(bit2, x)
+#             ans += trip
+#             update(bit2, x, val)
+#             update(bit1, x, 1)
+#         return ans
+#
+#
+# nums1 = [2,0,1,3]
+# nums2 = [0,1,2,3]
+# solution = Solution()
+# result = solution.goodTriplets(nums1, nums2)
+# print(result)
 
 
 # class Solution:
