7b7b27f4ae8c5f5ed7eb3211961b09ca251688c0
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-05-07 19:52:04 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -17,14 +17,49 @@ from typing import List, Optional
 
 
 class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[nums[i]] for i in range(len(nums))]
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n = len(moveTime)
+        m = len(moveTime[0])
+        INF = 10 ** 18
+        dist = [[INF] * m for _ in range(n)]
+        pq = [(0, 0, 0)]
+        dist[0][0] = 0
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while pq:
+            t, i, j = heapq.heappop(pq)
+            if t > dist[i][j]:
+                continue
+
+            if i == n - 1 and j == m - 1:
+                return t
+
+            for di, dj in dirs:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < n and 0 <= nj < m:
+                    new_time = max(t, moveTime[ni][nj]) + 1
+                    if new_time < dist[ni][nj]:
+                        dist[ni][nj] = new_time
+                        heapq.heappush(pq, (new_time, ni, nj))
+
+        return dist[n - 1][m - 1]
 
 
-nums = [0, 2, 1, 5, 3, 4]
+moveTime = [[0, 4], [4, 4]]
 solution = Solution()
-result = solution.buildArray(nums)
+result = solution.minTimeToReach(moveTime)
 print(result)
+
+
+# class Solution:
+#     def buildArray(self, nums: List[int]) -> List[int]:
+#         return [nums[nums[i]] for i in range(len(nums))]
+#
+#
+# nums = [0, 2, 1, 5, 3, 4]
+# solution = Solution()
+# result = solution.buildArray(nums)
+# print(result)
 
 
 # class Solution:
