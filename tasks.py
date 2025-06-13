2c33e7e37ce7e3cfcc913d0ebb7991d5d56acaa8
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-06-13 13:07:39 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -20,14 +20,45 @@ from numpy.ma.core import bitwise_or
 
 
 class Solution:
-    def maxAdjacentDistance(self, nums: List[int]) -> int:
-        return max(abs(v - nums[k - 1]) for k, v in enumerate(nums))
+    def minimizeMax(self, nums, p):
+        nums.sort()
+
+        def isValid(diff):
+            cnt, i = 0, 1
+            while i < len(nums):
+                if nums[i] - nums[i - 1] <= diff:
+                    cnt += 1
+                    i += 1
+                i += 1
+            return cnt >= p
+
+        l, r = 0, nums[-1] - nums[0]
+        while l <= r:
+            mid = (l + r) // 2
+            if isValid(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return ans
 
 
-nums = [1, 2, 4]
+nums = [10,1,2,7,1,3]
+p = 2
 solution = Solution()
-result = solution.maxAdjacentDistance(nums)
+result = solution.minimizeMax(nums, p)
 print(result)
+
+
+# class Solution:
+#     def maxAdjacentDistance(self, nums: List[int]) -> int:
+#         return max(abs(v - nums[k - 1]) for k, v in enumerate(nums))
+#
+#
+# nums = [1, 2, 4]
+# solution = Solution()
+# result = solution.maxAdjacentDistance(nums)
+# print(result)
 
 
 # class Solution:
