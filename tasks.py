2d6dd81e227de9dc2cd705e7a0f92af4ee097960
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-06-18 18:17:59 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -20,35 +20,53 @@ from numpy.ma.core import bitwise_or
 
 
 class Solution:
-    def countGoodArrays(self, n: int, m: int, k: int) -> int:
-        mod=10 ** 9 + 7
-        maxn = n
-        fact = [1] * (maxn + 1)
-        invfact = [1] * (maxn + 1)
-
-        for i in range(1, maxn + 1):
-            fact[i] = fact[i - 1] * i % mod
-        invfact[maxn] = pow(fact[maxn], mod-2, mod)
-
-        for i in range(maxn -1, -1, -1):
-            invfact[i] = invfact[i + 1] * (i + 1) % mod
-
-        def comb(a, b):
-            if b < 0 or b > a:
-                return 0
-            return fact[a] * invfact[b] % mod * invfact[a - b] % mod
-
-        ways = comb(n - 1, k) * m % mod
-        ways = ways * pow(m - 1, n - 1 - k, mod) % mod
-        return ways
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums) - 2, 3):
+            if nums[i + 2] - nums[i] > k:
+                return []
+            ans.append(nums[i:i + 3])
+        return ans
 
 
-n = 3
-m = 2
-k = 1
+nums = [1, 3, 4, 8, 7, 9, 3, 5, 1]
+k = 2
 solution = Solution()
-result = solution.countGoodArrays(n, m, k)
+result = solution.divideArray(nums, k)
 print(result)
+
+
+# class Solution:
+#     def countGoodArrays(self, n: int, m: int, k: int) -> int:
+#         mod=10 ** 9 + 7
+#         maxn = n
+#         fact = [1] * (maxn + 1)
+#         invfact = [1] * (maxn + 1)
+#
+#         for i in range(1, maxn + 1):
+#             fact[i] = fact[i - 1] * i % mod
+#         invfact[maxn] = pow(fact[maxn], mod-2, mod)
+#
+#         for i in range(maxn -1, -1, -1):
+#             invfact[i] = invfact[i + 1] * (i + 1) % mod
+#
+#         def comb(a, b):
+#             if b < 0 or b > a:
+#                 return 0
+#             return fact[a] * invfact[b] % mod * invfact[a - b] % mod
+#
+#         ways = comb(n - 1, k) * m % mod
+#         ways = ways * pow(m - 1, n - 1 - k, mod) % mod
+#         return ways
+#
+#
+# n = 3
+# m = 2
+# k = 1
+# solution = Solution()
+# result = solution.countGoodArrays(n, m, k)
+# print(result)
 
 
 # class Solution:
