7ca173f9ee92366e573fc0796ec02d85988ccdd9
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-06-30 18:42:17 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -20,31 +20,42 @@ import bisect
 
 
 class Solution:
-    def numSubseq(self, nums, target):
-        mod = 10 ** 9 + 7
-        nums.sort()
-        n = len(nums)
-        power = [1] * n
-        for i in range(1, n):
-            power[i] = (power[i - 1] * 2) % mod
-
-        left, right = 0, n - 1
-        result = 0
-        while left <= right:
-            if nums[left] + nums[right] <= target:
-                result = (result + power[right - left]) % mod
-                left += 1
-            else:
-                right -= 1
-
-        return result
+    def findLHS(self, nums: List[int]) -> int:
+        return (z:=Counter(nums)) and max(z[v + 1] and z[v] + z[v + 1] for v in z)
 
 
-nums = [3, 5, 6, 7]
-target = 9
+nums = [1, 3, 2, 2, 5, 2, 3, 7]
 solution = Solution()
-result = solution.numSubseq(nums, target)
+result = solution.findLHS(nums)
 print(result)
+
+
+# class Solution:
+#     def numSubseq(self, nums, target):
+#         mod = 10 ** 9 + 7
+#         nums.sort()
+#         n = len(nums)
+#         power = [1] * n
+#         for i in range(1, n):
+#             power[i] = (power[i - 1] * 2) % mod
+#
+#         left, right = 0, n - 1
+#         result = 0
+#         while left <= right:
+#             if nums[left] + nums[right] <= target:
+#                 result = (result + power[right - left]) % mod
+#                 left += 1
+#             else:
+#                 right -= 1
+#
+#         return result
+#
+#
+# nums = [3, 5, 6, 7]
+# target = 9
+# solution = Solution()
+# result = solution.numSubseq(nums, target)
+# print(result)
 
 
 # class Solution:
