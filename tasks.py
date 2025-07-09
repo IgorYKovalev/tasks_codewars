7b97c0969030fdde577b889010be3b999568f3b0
IgorYKovalev <121068451+IgorYKovalev@users.noreply.github.com>
2025-07-09 21:18:47 +0300

    add task

--- tasks.py
+++ tasks.py
@@ -20,34 +20,61 @@ import bisect
 
 
 class Solution:
-    def maxValue(self, events: List[List[int]], k: int) -> int:
-        N = len(events)
-        events.sort()
-        memo = [[-1 for _ in range(k + 1)] for _ in range(N)]
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        n, busy = len(startTime), 0
+        for i in range(k):
+            busy += endTime[i] - startTime[i]
 
-        def max_events_recur(idx, k):
-            if idx == N or k == 0:
-                return 0
-
-            if memo[idx][k] != -1:
-                return memo[idx][k]
-
-            startDay, endDay, value = events[idx]
-            nextIdxAvail = bisect.bisect_right(events, endDay, key=lambda event: event[0])
-            total = value + max_events_recur(nextIdxAvail, k - 1)
-            total = max(total, max_events_recur(idx + 1, k))
-            memo[idx][k] = total
-            return total
-
-        total = max_events_recur(0, k)
-        return total
+        if n == k: return eventTime - busy
+        ans = startTime[k] - busy
+        l = 0
+        for r in range(k, n):
+            busy += (endTime[r] - startTime[r]) - (endTime[l] - startTime[l])
+            end = eventTime if r == n - 1 else startTime[r + 1]
+            start = endTime[l]
+            ans = max(ans, end - start - busy)
+            l += 1
+        return ans
 
 
-events = [[1, 2, 4], [3, 4, 3], [2, 3, 1]]
-k = 2
+eventTime = 5
+k = 1
+startTime = [1, 3]
+endTime = [2, 5]
 solution = Solution()
-result = solution.maxValue(events, k)
+result = solution.maxFreeTime(eventTime, k, startTime, endTime)
 print(result)
+
+
+# class Solution:
+#     def maxValue(self, events: List[List[int]], k: int) -> int:
+#         N = len(events)
+#         events.sort()
+#         memo = [[-1 for _ in range(k + 1)] for _ in range(N)]
+#
+#         def max_events_recur(idx, k):
+#             if idx == N or k == 0:
+#                 return 0
+#
+#             if memo[idx][k] != -1:
+#                 return memo[idx][k]
+#
+#             startDay, endDay, value = events[idx]
+#             nextIdxAvail = bisect.bisect_right(events, endDay, key=lambda event: event[0])
+#             total = value + max_events_recur(nextIdxAvail, k - 1)
+#             total = max(total, max_events_recur(idx + 1, k))
+#             memo[idx][k] = total
+#             return total
+#
+#         total = max_events_recur(0, k)
+#         return total
+#
+#
+# events = [[1, 2, 4], [3, 4, 3], [2, 3, 1]]
+# k = 2
+# solution = Solution()
+# result = solution.maxValue(events, k)
+# print(result)
 
 
 # class FindSumPairs:
